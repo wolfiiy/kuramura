a3910858b3e681022b8d946aedcb27603c4175c8
wolfiiy <wolfiy@protonmail.ch>
2024-11-17 21:38:47 +0100

    🚸 Added logo
    Added a logo that can be clicked to go to the home page.

--- app/views/partials/head.php
+++ app/views/partials/head.php
@@ -4,5 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo isset($pageTitle) ? $pageTitle : "KuraMura"; ?></title>
+    <link rel="stylesheet" href="assets/css/main.css">
 </head>
 <body>
--- app/views/partials/nav.php
+++ app/views/partials/nav.php
@@ -1,16 +1,22 @@
 <header>
     <nav>
-        <ul>
-            <li>
-                <a href="index.php">Home</a>
-            </li>
-            <li>
-                <a href="add.php">Add</a>
-            </li>
-            <li>
-                <a href="search.php">Search</a>
-            </li>
-        </ul>
+        <div class="nav-container">
+            <span class="brand">
+                <a href="index.php">蔵村</a>
+            </span>
+    
+            <ul>
+                <li>
+                    <a href="index.php">Home</a>
+                </li>
+                <li>
+                    <a href="add.php">Add</a>
+                </li>
+                <li>
+                    <a href="search.php">Search</a>
+                </li>
+            </ul>
+        </div>
 
         <button>
             Login
