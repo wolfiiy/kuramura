4b63d93fee619561a5d2ed8339351fef9e4b23b8
wolfiiy <wolfiy@protonmail.ch>
2024-11-17 20:57:40 +0100

    💬 Reworked introduction
    Improved the greeting text of the home page.

---
+++ app/views/indexView.php
@@ -0,0 +1,17 @@
+<main>
+    <h1>KuraMura</h1>
+    <p>
+        Welcome to KuraMura! 
+    </p>
+
+    <p>
+        Most people own a lot of things. KuraMura is here to help you organize 
+        by keeping track of where your posessions are.
+    </p>
+
+    <p>
+        KuraMura gets its name from two Japanese words: 蔵 (kura, warehouse) and 
+        村 (mura, village). Think of this application as a small village for 
+        your things.
+    </p>
+</main>
